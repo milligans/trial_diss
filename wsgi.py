b87734c4404235e5e7104b0bd77a512c09ba553d
korova_blue@hotmail.com <73536360+milligans@users.noreply.github.com>
2021-06-29 12:00:09 +0100

    added main file and did some adjustments

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
+from app.main import app
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
+if __name__="__main__":
     app.run()
